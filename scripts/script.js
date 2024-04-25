9891384209a9bb3633746024530a326adb4073f7
Sean Shmulevich <shmul@Ahava.local>
2024-04-25 01:33:38 -0400

    get involved page is more bussing now, many styles written

--- scripts/script.js
+++ scripts/script.js
@@ -34,26 +34,29 @@ document.addEventListener('DOMContentLoaded', function () {
   // showSlides();
 
   var featuredMoreInfo = document.querySelector('.featured_more_info');
-  featuredMoreInfo.addEventListener('click', function () {
-    // Add your code here to handle the click event
+  if (featuredMoreInfo !== null) {
 
-    let featuredItems = document.querySelector('.featured_items');
+    featuredMoreInfo.addEventListener('click', function () {
+      // Add your code here to handle the click event
 
-    let newCarat = document.createElement('span');
-    newCarat.className = 'carat';
-    if (featuredItems.style.display === "flex") {
-      featuredItems.style.display = "none";
-      featuredMoreInfo.textContent = "more info";
-      newCarat.textContent = "⌄";
-      featuredMoreInfo.appendChild(newCarat);
-    }
-    else {
-      featuredMoreInfo.textContent = "less info";
-      newCarat.textContent = "⌃";
-      featuredMoreInfo.appendChild(newCarat);
-      featuredItems.style.display = "flex";
-    }
-  }, true);
+      let featuredItems = document.querySelector('.featured_items');
+
+      let newCarat = document.createElement('span');
+      newCarat.className = 'carat';
+      if (featuredItems.style.display === "flex") {
+        featuredItems.style.display = "none";
+        featuredMoreInfo.textContent = "more info";
+        newCarat.textContent = "⌄";
+        featuredMoreInfo.appendChild(newCarat);
+      }
+      else {
+        featuredMoreInfo.textContent = "less info";
+        newCarat.textContent = "⌃";
+        featuredMoreInfo.appendChild(newCarat);
+        featuredItems.style.display = "flex";
+      }
+    }, true);
+  }
 
   let modalClose = document.querySelector('.close_modal');
   let modalOpen = document.querySelector('#open_modal');
@@ -61,27 +64,47 @@ document.addEventListener('DOMContentLoaded', function () {
   let cameraButton = document.querySelector('#trackbutton');
   let cameraOn = false;
 
-  cameraButton.addEventListener('click', function () {
-    cameraOn = !cameraOn;
-    if (cameraOn) {
-      cameraButton.textContent = "Disable Camera";
-      cameraButton.style.backgroundColor = "red";
-      modalOpen.textContent = "Interactive Demo Active";
-      modalOpen.style.color = "#4caf50";
-    }
-    else {
-      cameraButton.textContent = "Enable Camera";
-      cameraButton.style.backgroundColor = "#4caf50";
-      modalOpen.textContent = "Try our Interactive Demo";
-      modalOpen.style.color = "rgba(255, 255, 255, 0.65)";
-    }
-  });
+  if (modalClose && modalOpen && modal) {
+    cameraButton.addEventListener('click', function () {
+      cameraOn = !cameraOn;
+      if (cameraOn) {
+        cameraButton.textContent = "Disable Camera";
+        cameraButton.style.backgroundColor = "red";
+        modalOpen.textContent = "Interactive Demo Active";
+        modalOpen.style.color = "#4caf50";
+      }
+      else {
+        cameraButton.textContent = "Enable Camera";
+        cameraButton.style.backgroundColor = "#4caf50";
+        modalOpen.textContent = "Try our Interactive Demo";
+        modalOpen.style.color = "rgba(255, 255, 255, 0.65)";
+      }
+    });
 
-  modalOpen.addEventListener('click', function () {
-    modal.style.display = "block";
+    modalOpen.addEventListener('click', function () {
+      modal.style.display = "block";
 
-  });
-  modalClose.addEventListener('click', function () {
-    modal.style.display = "none";
-  });
+    });
+    modalClose.addEventListener('click', function () {
+      modal.style.display = "none";
+    });
+  }
+
+
+  function adjustTextareaHeight() {
+    let textarea = document.getElementById('form_text_area');
+    textarea.style.height = 'auto';  // Reset the height
+
+    textarea.style.height = textarea.scrollHeight + 'px';  // Set the height to scroll height
+  }
+
+  // Attach the function to the input event of the textarea
+  var textarea = document.getElementById('form_text_area');
+
+  if (textarea) {
+    textarea.addEventListener('input', adjustTextareaHeight);
+    // Call the function initially to adjust to the default text
+    adjustTextareaHeight();
+  }
+
 });
